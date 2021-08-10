0a660e657c4697715ea8224b4a315c7b08996b79
Kyle Corry <kylecorry31@gmail.com>
2021-08-10 16:59:51 -0400

    Add fragments
    Closes #18

---
+++ fragments/src/main/java/com/kylecorry/andromeda/fragments/BoundFragment.kt
@@ -0,0 +1,34 @@
+package com.kylecorry.andromeda.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+
+abstract class BoundFragment<T : ViewBinding> : AndromedaFragment() {
+
+    abstract fun generateBinding(layoutInflater: LayoutInflater, container: ViewGroup?): T
+
+    protected val binding: T
+        get() = _binding!!
+
+    protected val isBound: Boolean
+        get() = context != null && _binding != null
+
+    private var _binding: T? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = generateBinding(inflater, container)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
