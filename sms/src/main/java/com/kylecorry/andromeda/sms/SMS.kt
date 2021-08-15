536f3980671367cebfde0722b60ca7f2a68cb20b
Kyle Corry <kylecorry31@gmail.com>
2021-08-15 10:40:04 -0400

    Convert SMS to an object

--- sms/src/main/java/com/kylecorry/andromeda/sms/SMS.kt
+++ sms/src/main/java/com/kylecorry/andromeda/sms/SMS.kt
@@ -4,12 +4,17 @@ import android.Manifest
 import android.telephony.SmsManager
 import androidx.annotation.RequiresPermission
 
-class SmsService {
+object SMS {
 
     @RequiresPermission(Manifest.permission.SEND_SMS)
     fun send(to: String, message: String) {
         val sms = SmsManager.getDefault()
-        sms.sendTextMessage(to, null, message, null, null)
+        val parts = sms.divideMessage(message)
+        if (parts.size <= 1) {
+            sms.sendTextMessage(to, null, message, null, null)
+        } else {
+            sms.sendMultipartTextMessage(to, null, parts, null, null)
+        }
     }
 
 }
