0d692679325c22ac4d250807b69ac0d12118508f
Kyle Corry <kylecorry31@gmail.com>
2021-08-30 14:38:33 -0400

    Improve logging

--- jobs/src/main/java/com/kylecorry/andromeda/jobs/DailyJobReceiver.kt
+++ jobs/src/main/java/com/kylecorry/andromeda/jobs/DailyJobReceiver.kt
@@ -58,7 +58,7 @@ abstract class DailyJobReceiver(
             if (isAfterWindow || (inWindow && !shouldSend)) {
                 Log.d(
                     "ScheduledJobReceiver",
-                    "${this::class.java.simpleName} received a broadcast too late"
+                    "${this::class.java.simpleName} received a broadcast too late or it already ran today"
                 )
                 setAlarm(context, tomorrowSendTime)
             }
