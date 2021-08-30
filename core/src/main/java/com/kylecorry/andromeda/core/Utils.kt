e71cb2839caf903c251de380bb6951a07ee4854c
Kyle Corry <kylecorry31@gmail.com>
2021-08-30 12:19:58 -0400

    Add string helper methods

--- core/src/main/java/com/kylecorry/andromeda/core/Utils.kt
+++ core/src/main/java/com/kylecorry/andromeda/core/Utils.kt
@@ -2,6 +2,7 @@ package com.kylecorry.andromeda.core
 
 import android.util.Range
 import com.kylecorry.andromeda.core.specifications.Specification
+import java.util.*
 
 fun tryOrNothing(block: () -> Unit) {
     try {
@@ -26,4 +27,13 @@ fun <T> List<T>.filterNotSatisfied(spec: Specification<T>): List<T> {
 
 fun <T> List<T>.filterIndices(indices: List<Int>): List<T> {
     return filterIndexed { index, _ -> indices.contains(index) }
+}
+
+fun String.capitalizeCompat(): String {
+    return replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+}
+
+fun String.capitalizeWords(): String {
+    val words = split(" ")
+    return words.joinToString(" ") { it.capitalizeCompat() }
 }
