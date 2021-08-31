d1737a239553fe28e069bcc6a00bb3083bc83436
Kyle Corry <kylecorry31@gmail.com>
2021-08-31 17:37:12 -0400

    Add intent extras to alarm broadcast scheduler

--- jobs/src/main/java/com/kylecorry/andromeda/jobs/AlarmBroadcastTaskScheduler.kt
+++ jobs/src/main/java/com/kylecorry/andromeda/jobs/AlarmBroadcastTaskScheduler.kt
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.os.Bundle
 import java.time.Duration
 import java.time.Instant
 import java.time.LocalDateTime
@@ -14,7 +15,8 @@ class AlarmBroadcastTaskScheduler(
     private val receiver: Class<out BroadcastReceiver>,
     private val uniqueId: Int,
     private val exact: Boolean = true,
-    private val allowWhileIdle: Boolean = false
+    private val allowWhileIdle: Boolean = false,
+    private val intentExtras: Bundle? = null
 ) :
     ITaskScheduler {
 
@@ -38,10 +40,15 @@ class AlarmBroadcastTaskScheduler(
     }
 
     private fun createPendingIntent(): PendingIntent {
+        val intent = Intent(context, receiver)
+        if (intentExtras != null) {
+            intent.putExtras(intentExtras)
+        }
+
         return PendingIntent.getBroadcast(
             context,
             uniqueId,
-            Intent(context, receiver),
+            intent,
             PendingIntent.FLAG_UPDATE_CURRENT or if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0
         )
     }
