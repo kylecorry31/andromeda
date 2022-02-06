88fa5c74fba49546305fdafd4e8a9ffec8d7c235
Kyle Corry <kylecorry31@gmail.com>
2022-02-06 06:47:32 -0500

    Update bluetooth device interfaces

---
+++ bluetooth/src/main/java/com/kylecorry/andromeda/bluetooth/IBluetoothDevice.kt
@@ -0,0 +1,30 @@
+package com.kylecorry.andromeda.bluetooth
+
+import android.annotation.SuppressLint
+import java.io.InputStream
+import java.io.OutputStream
+
+interface IBluetoothDevice {
+    val address: String
+    val name: String
+
+    // TODO: Let the consumer know it requires a permission
+    @SuppressLint("MissingPermission")
+    fun connect()
+    fun disconnect()
+    fun isConnected(): Boolean
+
+    // Reading
+    fun read(): Int
+    fun readUntil(predicate: (char: Char) -> Boolean): String
+    fun readUntil(stop: Char): String
+    fun readLine(): String
+
+    // Writing
+    fun write(str: String)
+    fun write(bytes: ByteArray)
+    fun write(byte: Byte)
+
+    fun getInputStream(): InputStream?
+    fun getOutputStream(): OutputStream?
+}
---
+++ bluetooth/src/main/java/com/kylecorry/andromeda/bluetooth/InsecureBluetoothDevice.kt
@@ -0,0 +1,14 @@
+package com.kylecorry.andromeda.bluetooth
+
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.content.Context
+import java.util.*
+
+class InsecureBluetoothDevice(context: Context, address: String, private val uuid: UUID) : BaseBluetoothDevice(context, address) {
+    @SuppressLint("MissingPermission")
+    override fun getSocket(device: BluetoothDevice): BluetoothSocket {
+        return device.createInsecureRfcommSocketToServiceRecord(uuid)
+    }
+}
