f52fe043e0b0192e08ec15278eea03931bc25d5b
Kyle Corry <kylecorry31@gmail.com>
2022-07-24 06:53:56 -0400

    Remove code duplication in coroutine timer

--- core/src/main/java/com/kylecorry/andromeda/core/time/CoroutineTimer.kt
+++ core/src/main/java/com/kylecorry/andromeda/core/time/CoroutineTimer.kt
@@ -1,24 +1,14 @@
 package com.kylecorry.andromeda.core.time
 
-import android.os.Handler
-import android.os.Looper
-import android.os.SystemClock
 import com.kylecorry.andromeda.core.coroutines.ControlledRunner
-import com.kylecorry.andromeda.core.tryOrNothing
+import com.kylecorry.andromeda.core.tryOrLog
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import java.time.Duration
 
 class CoroutineTimer(private val scope: CoroutineScope, private val action: suspend () -> Any) {
 
-    private var running = false
-    private val handler = Handler(Looper.getMainLooper())
-
-    private var intervalRunnable: Runnable? = null
-
-    private val runner = ControlledRunner<Any>()
-
-    private val runnable = Runnable {
+    private val timer = Timer {
         scope.launch {
             runner.joinPreviousOrRun {
                 action()
@@ -26,70 +16,33 @@ class CoroutineTimer(private val scope: CoroutineScope, private val action: susp
         }
     }
 
+    private val runner = ControlledRunner<Any>()
 
     fun interval(periodMillis: Long, initialDelayMillis: Long = 0L) {
-        if (running) {
-            stop()
-        }
-
-        running = true
-
-        val r = Runnable {
-            runnable.run()
-            val nextRunnable = intervalRunnable
-            if (nextRunnable != null) {
-                handler.postAtTime(nextRunnable, SystemClock.uptimeMillis() + periodMillis)
-            }
-        }
-
-        intervalRunnable = r
-
-        if (initialDelayMillis == 0L) {
-            handler.post(r)
-        } else {
-            handler.postAtTime(r, SystemClock.uptimeMillis() + initialDelayMillis)
-        }
+        timer.interval(periodMillis, initialDelayMillis)
     }
 
     fun interval(period: Duration, initialDelay: Duration = Duration.ZERO) {
-        interval(period.toMillis(), initialDelay.toMillis())
+        timer.interval(period, initialDelay)
     }
 
     fun once(delayMillis: Long) {
-        if (running) {
-            stop()
-        }
-
-        running = true
-        if (delayMillis == 0L) {
-            handler.post(runnable)
-        } else {
-            handler.postAtTime(runnable, SystemClock.uptimeMillis() + delayMillis)
-        }
+        timer.once(delayMillis)
     }
 
     fun once(delay: Duration) {
-        once(delay.toMillis())
+        timer.once(delay)
     }
 
     fun stop() {
-        val iRunnable = intervalRunnable
-        if (iRunnable != null) {
-            handler.removeCallbacks(iRunnable)
-        }
-        intervalRunnable = null
-
-        handler.removeCallbacks(runnable)
-
-        tryOrNothing {
+        timer.stop()
+        tryOrLog {
             runner.cancel()
         }
-
-        running = false
     }
 
     fun isRunning(): Boolean {
-        return running
+        return timer.isRunning()
     }
 
 }
