47e30dcdec1cc2ca33f532f7e63ef330b8eeb298
Kyle Corry <kylecorry31@gmail.com>
2023-01-20 12:52:01 -0500

    Get URI file name from content resolver

--- files/src/main/java/com/kylecorry/andromeda/files/ExternalFileSystem.kt
+++ files/src/main/java/com/kylecorry/andromeda/files/ExternalFileSystem.kt
@@ -2,6 +2,8 @@ package com.kylecorry.andromeda.files
 
 import android.content.Context
 import android.net.Uri
+import android.provider.OpenableColumns
+import com.kylecorry.andromeda.core.tryOrLog
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.InputStream
@@ -63,17 +65,43 @@ class ExternalFileSystem(private val context: Context) {
         return context.contentResolver.getType(uri)
     }
 
-    fun getFileName(uri: Uri, withExtension: Boolean = true): String? {
-        val name = uri.lastPathSegment ?: return null
+    fun getFileName(
+        uri: Uri,
+        withExtension: Boolean = true,
+        fallbackToPath: Boolean = true
+    ): String? {
+
+        var name: String? = null
+        tryOrLog {
+            val cursor = context.contentResolver.query(
+                uri,
+                arrayOf(OpenableColumns.DISPLAY_NAME),
+                null,
+                null,
+                null
+            )
+            cursor?.use {
+                val idx = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                if (idx >= 0) {
+                    it.moveToFirst()
+                    name = it.getString(idx)
+                }
+            }
+        }
+
+        if (name == null && fallbackToPath) {
+            name = uri.lastPathSegment
+        }
+
         return if (withExtension) {
             name
         } else {
-            name.substringBeforeLast(".")
+            name?.substringBeforeLast(".")
         }
     }
 
     fun getExtension(uri: Uri): String? {
-        val name = getFileName(uri, true) ?: return null
+        val name = getFileName(uri, withExtension = true, fallbackToPath = true) ?: return null
         return name.substringAfterLast(".")
     }
 }
