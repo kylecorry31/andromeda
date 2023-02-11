f6b1f7949e151231b8c151519ec77f7f34b30223
Kyle Corry <kylecorry31@gmail.com>
2023-02-11 07:26:50 -0500

    Fix location listener on older android versions

--- location/src/main/java/com/kylecorry/andromeda/location/SimpleLocationListener.kt
+++ location/src/main/java/com/kylecorry/andromeda/location/SimpleLocationListener.kt
@@ -2,10 +2,15 @@ package com.kylecorry.andromeda.location
 
 import android.location.Location
 import android.location.LocationListener
+import android.os.Bundle
 
 internal class SimpleLocationListener(private val onLocationChangedFn: (location: Location?) -> Unit): LocationListener {
     override fun onLocationChanged(location: Location) {
         onLocationChangedFn.invoke(location)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
     }
 
     override fun onProviderDisabled(provider: String) {
