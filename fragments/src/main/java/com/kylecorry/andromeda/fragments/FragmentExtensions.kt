9777bbc02d7f299beb240c12ff76fb84ff6953e7
Kyle Corry <kylecorry31@gmail.com>
2023-03-26 08:43:38 -0400

    Use recommended replacement for launchWhen

--- fragments/src/main/java/com/kylecorry/andromeda/fragments/FragmentExtensions.kt
+++ fragments/src/main/java/com/kylecorry/andromeda/fragments/FragmentExtensions.kt
@@ -12,7 +12,9 @@ import androidx.fragment.app.commit
 import androidx.lifecycle.*
 import com.google.android.material.color.DynamicColors
 import com.kylecorry.andromeda.core.coroutines.BackgroundMinimumState
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 
 fun Fragment.switchToFragment(
@@ -57,51 +59,57 @@ fun Application.useDynamicColors() {
 fun LifecycleOwner.inBackground(
     state: BackgroundMinimumState = BackgroundMinimumState.Resumed,
     cancelWhenBelowState: Boolean = true,
-    throwOnDestroy: Boolean = false,
     block: suspend CoroutineScope.() -> Unit
 ) {
     val minimumState = when (state) {
         BackgroundMinimumState.Resumed -> Lifecycle.State.RESUMED
         BackgroundMinimumState.Started -> Lifecycle.State.STARTED
         BackgroundMinimumState.Created -> Lifecycle.State.CREATED
-        BackgroundMinimumState.Any -> Lifecycle.State.INITIALIZED
+        BackgroundMinimumState.Any -> null
     }
 
     lifecycleScope.launch {
-        val scope = this
-        waitUntilState(minimumState, true, throwOnDestroy) {
-            if (cancelWhenBelowState) {
-                block(scope)
-            }
+
+        // If there is no minimum state, just run the block
+        if (minimumState == null) {
+            block()
+            return@launch
         }
 
+
         if (!cancelWhenBelowState) {
+            // The block will just run until completion once the state is reached
+            waitUntilState(minimumState, throwOnCancel = false)
             block()
+        } else {
+            // The block will be cancelled if the state falls below the minimum state
+            // Need to use repeat on lifecycle because there's no better way to do this anymore
+            var isComplete = false
+            repeatOnLifecycle(minimumState) {
+                if (!isComplete) {
+                    try {
+                        block()
+                    } finally {
+                        isComplete = true
+                        this.cancel()
+                    }
+                }
+            }
         }
     }
 }
 
 suspend inline fun LifecycleOwner.waitUntilState(
     state: Lifecycle.State,
-    unchecked: Boolean = false,
-    throwOnDestroy: Boolean = false,
-    crossinline block: suspend () -> Unit
+    throwOnCancel: Boolean = false,
+    crossinline block: () -> Unit = {}
 ) {
-    // The minimum state for withStateAtLeast is CREATED, or else it will throw
-    // If we are waiting for a state below CREATED, we should just run it if unchecked
-    if (unchecked && state < Lifecycle.State.CREATED) {
-        return block()
-    }
-
     try {
         withStateAtLeast(state) {
-            // I'm not sure why this can't run a suspend function - maybe they'll change it in the future or provide more documentation
-            lifecycleScope.launch {
-                block()
-            }
+            block()
         }
-    } catch (e: LifecycleDestroyedException) {
-        if (throwOnDestroy) {
+    } catch (e: CancellationException) {
+        if (throwOnCancel) {
             throw e
         }
     }
