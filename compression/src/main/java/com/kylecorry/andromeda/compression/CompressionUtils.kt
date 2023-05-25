e4fc863009af5fc8b9e328a8c5736ac96eb1459e
Kyle Corry <kylecorry31@gmail.com>
2023-05-25 16:55:58 -0400

    Add decompression on the bit level

--- compression/src/main/java/com/kylecorry/andromeda/compression/CompressionUtils.kt
+++ compression/src/main/java/com/kylecorry/andromeda/compression/CompressionUtils.kt
@@ -6,33 +6,60 @@ object CompressionUtils {
 
     @OptIn(ExperimentalUnsignedTypes::class)
     fun getShort(stream: InputStream, line: Int, closeStream: Boolean = true): Short? {
-        val bytes = getBytes(stream, line, Short.SIZE_BYTES, closeStream) ?: return null
+        val bytes =
+            getBytes(stream, line, Short.SIZE_BYTES, closeStream = closeStream) ?: return null
         return bytes.toShort()
     }
 
     @OptIn(ExperimentalUnsignedTypes::class)
     fun getInt(stream: InputStream, line: Int, closeStream: Boolean = true): Int? {
-        val bytes = getBytes(stream, line, Int.SIZE_BYTES, closeStream) ?: return null
+        val bytes = getBytes(stream, line, Int.SIZE_BYTES, closeStream = closeStream) ?: return null
         return bytes.toInt()
     }
 
-    fun getByte(stream: InputStream, line: Int, closeStream: Boolean = true): Byte? {
-        val bytes = getBytes(stream, line, 1, closeStream) ?: return null
-        return bytes[0]
+    fun getByte(
+        stream: InputStream,
+        line: Int,
+        numBits: Int = Byte.SIZE_BITS,
+        closeStream: Boolean = true
+    ): Byte? {
+        if (numBits > Byte.SIZE_BITS) throw IllegalArgumentException("Cannot read more than 8 bits")
+
+        if (numBits == Byte.SIZE_BITS) {
+            return getBytes(stream, line, 1, closeStream = closeStream)?.get(0)
+        }
+
+        val startBit = line * numBits
+        val startByte = startBit / 8
+        val startingOffset = startBit % 8
+
+        val secondLineNeeded = (startBit + numBits) / 8 > startByte
+
+        val bytes =
+            getBytes(
+                stream,
+                startByte,
+                1,
+                lineCount = if (secondLineNeeded) 2 else 1,
+                closeStream = closeStream
+            ) ?: return null
+
+        return bytes.bitsToByte(numBits, startingOffset)
     }
 
     fun getBytes(
         stream: InputStream,
         line: Int,
-        bytes: Int,
+        bytesPerLine: Int,
+        lineCount: Int = 1,
         closeStream: Boolean = true
     ): ByteArray? {
         var value: ByteArray?
         try {
-            val desiredLine = bytes.toLong() * line
+            val desiredLine = bytesPerLine.toLong() * line
             stream.skip(desiredLine)
-            value = ByteArray(bytes)
-            stream.read(value, 0, bytes)
+            value = ByteArray(bytesPerLine * lineCount.coerceAtLeast(1))
+            stream.read(value, 0, bytesPerLine * lineCount.coerceAtLeast(1))
         } catch (e: Exception) {
             value = null
         } finally {
