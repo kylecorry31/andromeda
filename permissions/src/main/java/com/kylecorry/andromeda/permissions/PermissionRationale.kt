5b63854d4764505a095cd53c34290b02ed97ba91
Kyle Corry <kylecorry31@gmail.com>
2023-07-04 08:42:54 -0400

    Swap parameter order

--- permissions/src/main/java/com/kylecorry/andromeda/permissions/PermissionRationale.kt
+++ permissions/src/main/java/com/kylecorry/andromeda/permissions/PermissionRationale.kt
@@ -3,6 +3,6 @@ package com.kylecorry.andromeda.permissions
 data class PermissionRationale(
     val title: String,
     val message: CharSequence,
-    val cancel: String? = null,
-    val ok: String? = null
+    val ok: String? = null,
+    val cancel: String? = null
 )
