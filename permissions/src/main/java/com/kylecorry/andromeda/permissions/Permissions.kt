64a87eb714edee877599d8fdf991610cda142c57
Kyle Corry <kylecorry31@gmail.com>
2023-07-15 14:05:13 -0400

    Expand check for foreground service permission

--- permissions/src/main/java/com/kylecorry/andromeda/permissions/Permissions.kt
+++ permissions/src/main/java/com/kylecorry/andromeda/permissions/Permissions.kt
@@ -3,10 +3,12 @@ package com.kylecorry.andromeda.permissions
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.AlarmManager
+import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
+import android.content.pm.ServiceInfo
 import android.net.Uri
 import android.os.Build
 import android.os.PowerManager
@@ -19,7 +21,10 @@ import com.kylecorry.andromeda.core.tryOrDefault
 
 object Permissions {
 
-    fun isBackgroundLocationEnabled(context: Context, requireFineLocation: Boolean = false): Boolean {
+    fun isBackgroundLocationEnabled(
+        context: Context,
+        requireFineLocation: Boolean = false
+    ): Boolean {
         return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
             (requireFineLocation && canGetFineLocation(context)) || canGetLocation(context)
         } else {
@@ -110,7 +115,9 @@ object Permissions {
     fun requestPermission(context: Context, permission: SpecialPermission) {
         when (permission) {
             SpecialPermission.SCHEDULE_EXACT_ALARMS -> requestScheduleExactAlarms(context)
-            SpecialPermission.IGNORE_BATTERY_OPTIMIZATIONS -> requestIgnoreBatteryOptimization(context)
+            SpecialPermission.IGNORE_BATTERY_OPTIMIZATIONS -> requestIgnoreBatteryOptimization(
+                context
+            )
         }
     }
 
@@ -163,7 +170,11 @@ object Permissions {
         context.startActivity(Intents.alarmAndReminderSettings(context))
     }
 
-    fun canCreateForegroundServices(context: Context, fromBackground: Boolean = false): Boolean {
+    fun canCreateForegroundServices(
+        context: Context,
+        fromBackground: Boolean = false,
+        foregroundServiceTypes: List<Int> = emptyList()
+    ): Boolean {
         // Nothing special was needed before Android P
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
             return true
@@ -180,10 +191,92 @@ object Permissions {
             return false
         }
 
-        // On Android S, the permission is not enough to create foreground services from the background
+        // On Android S+, the permission is not enough to create foreground services from the background
         // unless the app is ignoring battery optimizations
-        if (fromBackground) {
-            return isIgnoringBatteryOptimizations(context)
+        if (fromBackground && !isIgnoringBatteryOptimizations(context)) {
+            return false
+        }
+
+        // Only SDK 34+ requires a foreground service type check
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            return true
+        }
+
+        // Check the foreground service types
+        if (foregroundServiceTypes.isEmpty()) {
+            return true
+        }
+
+        val permissionMap = mapOf(
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_CAMERA to Manifest.permission.FOREGROUND_SERVICE_CAMERA,
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_CONNECTED_DEVICE to Manifest.permission.FOREGROUND_SERVICE_CONNECTED_DEVICE,
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC to Manifest.permission.FOREGROUND_SERVICE_DATA_SYNC,
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_HEALTH to Manifest.permission.FOREGROUND_SERVICE_HEALTH,
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION to Manifest.permission.FOREGROUND_SERVICE_LOCATION,
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK to Manifest.permission.FOREGROUND_SERVICE_MEDIA_PLAYBACK,
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PROJECTION to Manifest.permission.FOREGROUND_SERVICE_MEDIA_PROJECTION,
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_MICROPHONE to Manifest.permission.FOREGROUND_SERVICE_MICROPHONE,
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_PHONE_CALL to Manifest.permission.FOREGROUND_SERVICE_PHONE_CALL,
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_REMOTE_MESSAGING to Manifest.permission.FOREGROUND_SERVICE_REMOTE_MESSAGING,
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE to Manifest.permission.FOREGROUND_SERVICE_SPECIAL_USE,
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_SYSTEM_EXEMPTED to Manifest.permission.FOREGROUND_SERVICE_SYSTEM_EXEMPTED,
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_SHORT_SERVICE to null
+        )
+
+        // Verify the manifest permission is granted for each type
+        for (type in foregroundServiceTypes) {
+            val permission = permissionMap[type] ?: continue
+            if (!hasPermission(context, permission)) {
+                return false
+            }
+        }
+
+        // Extra permission checks for specific types
+        val runtimePermissions = mapOf(
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_CAMERA to listOf(Manifest.permission.CAMERA),
+            // TODO: Also if the user grants access to a USB device
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_CONNECTED_DEVICE to listOf(
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.CHANGE_NETWORK_STATE,
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.CHANGE_WIFI_MULTICAST_STATE,
+                Manifest.permission.NFC,
+                Manifest.permission.TRANSMIT_IR
+            ),
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC to null,
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_HEALTH to listOf(
+                Manifest.permission.ACTIVITY_RECOGNITION,
+                Manifest.permission.BODY_SENSORS,
+                Manifest.permission.HIGH_SAMPLING_RATE_SENSORS
+            ),
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION to listOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK to null,
+            // TODO: The user must have allowed screen capture for the app
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PROJECTION to null,
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_MICROPHONE to listOf(
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.CAPTURE_AUDIO_OUTPUT
+            ),
+            // TODO: Also if this is holding the default dialer role
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_PHONE_CALL to listOf(
+                Manifest.permission.MANAGE_OWN_CALLS
+            ),
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_REMOTE_MESSAGING to null,
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE to null,
+            // TODO: There are checks that can be done for this other than permissions
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_SYSTEM_EXEMPTED to null,
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_SHORT_SERVICE to null
+        )
+
+        for (type in foregroundServiceTypes) {
+            val permissions = runtimePermissions[type] ?: continue
+            // Any of the permissions must be granted
+            if (permissions.none { hasPermission(context, it) }) {
+                return false
+            }
         }
 
         return true
