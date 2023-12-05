6ffff33121e6b41bf7f737807d36c50b33444609
Kyle Corry <kylecorry31@gmail.com>
2023-12-05 07:59:33 -0500

    Readd clinometer angle

--- sense/src/main/java/com/kylecorry/andromeda/sense/clinometer/Clinometer.kt
+++ sense/src/main/java/com/kylecorry/andromeda/sense/clinometer/Clinometer.kt
@@ -3,6 +3,7 @@ package com.kylecorry.andromeda.sense.clinometer
 import com.kylecorry.andromeda.core.sensors.AbstractSensor
 import com.kylecorry.andromeda.sense.orientation.IOrientationSensor
 import com.kylecorry.andromeda.sense.orientation.OrientationUtils
+import com.kylecorry.sol.math.SolMath.normalizeAngle
 import com.kylecorry.sol.science.geology.Geology
 
 /**
@@ -18,6 +19,9 @@ class Clinometer(
     private var _angle = 0f
     private val rotationMatrix = FloatArray(16)
     private val orientation = FloatArray(3)
+
+    override val angle: Float
+        get() = _angle
 
     override val hasValidReading: Boolean
         get() = orientationSensor.hasValidReading
@@ -35,19 +39,35 @@ class Clinometer(
 
     private fun onSensorUpdate(): Boolean {
         _angle = if (isAugmentedReality) {
-            OrientationUtils.getAROrientation(
-                orientationSensor,
-                rotationMatrix,
-                orientation
-            )
-            orientation[1]
-        } else {
+            // First get the pitch using the compass orientation - this will tell us which quadrant we're in
             OrientationUtils.getCompassOrientation(
                 orientationSensor,
                 rotationMatrix,
                 orientation
             )
-            -orientation[1]
+            val pitch = orientation[1]
+
+            // Now get the AR orientation, and adjust it based on the quadrant
+            OrientationUtils.getAROrientation(
+                orientationSensor,
+                rotationMatrix,
+                orientation
+            )
+
+            if (pitch < 0f){
+                // Upright
+                normalizeAngle(orientation[1])
+            } else {
+                // Upside down
+                normalizeAngle(-orientation[1] - 180)
+            }
+        } else {
+            OrientationUtils.getAROrientation(
+                orientationSensor,
+                rotationMatrix,
+                orientation
+            )
+            normalizeAngle(-orientation[2] + 90)
         }
         notifyListeners()
         return true
