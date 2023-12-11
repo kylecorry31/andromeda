15af9414c77e40a1037c8ab74534f09538f57c1a
Kyle Corry <kylecorry31@gmail.com>
2023-12-11 08:55:07 -0500

    Add markdown linkify plugin

--- markdown/src/main/java/com/kylecorry/andromeda/markdown/MarkdownService.kt
+++ markdown/src/main/java/com/kylecorry/andromeda/markdown/MarkdownService.kt
@@ -3,22 +3,31 @@ package com.kylecorry.andromeda.markdown
 import android.content.Context
 import android.text.Spanned
 import android.widget.TextView
+import androidx.core.text.util.LinkifyCompat
 import io.noties.markwon.AbstractMarkwonPlugin
 import io.noties.markwon.Markwon
 import io.noties.markwon.MarkwonSpansFactory
 import io.noties.markwon.core.spans.LastLineSpacingSpan
+import io.noties.markwon.linkify.LinkifyPlugin
 import org.commonmark.node.ListItem
 
-class MarkdownService(private val context: Context) {
+class MarkdownService(private val context: Context, private val autoLink: Boolean = true) {
     private val markwon by lazy {
-        Markwon.builder(context).usePlugin(object : AbstractMarkwonPlugin() {
+        val builder = Markwon.builder(context)
+        builder.usePlugin(object : AbstractMarkwonPlugin() {
             override fun configureSpansFactory(builder: MarkwonSpansFactory.Builder) {
                 super.configureSpansFactory(builder)
                 builder.appendFactory(
                     ListItem::class.java
                 ) { _, _ -> LastLineSpacingSpan(16) }
             }
-        }).build()
+        })
+
+        if (autoLink) {
+            builder.usePlugin(LinkifyPlugin.create(true))
+        }
+
+        builder.build()
     }
 
     fun setMarkdown(view: TextView, markdown: String) {
