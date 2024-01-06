b6579c34963bf5a0baeadd5edcfa796d95069c9a
Kyle Corry <kylecorry31@gmail.com>
2024-01-06 09:17:47 -0500

    Add maven publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,9 +16,8 @@ buildscript {
 
 plugins {
     id("com.android.library") version "8.2.0" apply false
-    id("com.google.devtools.ksp") version "1.9.21-1.0.15" apply false
     id("org.jetbrains.kotlin.android") version "1.9.21" apply false
-    id("com.google.dagger.hilt.android") version "2.49" apply false
+    id("maven-publish")
 }
 
 task("clean") {
