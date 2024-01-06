81d7c0b6941736679c4bb0f257de3fb95319dd4e
Kyle Corry <kylecorry31@gmail.com>
2024-01-06 13:15:47 -0500

    Make quaternion filter more robust to NaN

--- sense/src/main/java/com/kylecorry/andromeda/sense/orientation/filter/LowPassOrientationSensorFilter.kt
+++ sense/src/main/java/com/kylecorry/andromeda/sense/orientation/filter/LowPassOrientationSensorFilter.kt
@@ -15,15 +15,32 @@ class LowPassOrientationSensorFilter(
 
     private val lock = Any()
     private val _current = Quaternion.zero.toFloatArray()
+    private val _last = Quaternion.zero.toFloatArray()
+
 
     override fun filter(quaternion: FloatArray, out: FloatArray) {
+        // Don't bother filtering if the quaternion is invalid
+        if (quaternion.any { it.isNaN() || it.isInfinite() }) return
+
         synchronized(lock) {
+            // Make a copy of the last value in case we need to roll back
+            _current.copyInto(_last)
+
+            // Low pass filter
             if (useSlerp) {
                 QuaternionMath.slerp(_current, quaternion, alpha, _current, true)
             } else {
                 QuaternionMath.lerp(_current, quaternion, alpha, _current, true)
             }
             QuaternionMath.normalize(_current, _current)
+
+
+            // If the new value is invalid, roll back
+            if (_current.any { it.isNaN() || it.isInfinite() }) {
+                _last.copyInto(_current)
+            }
+
+            // Write it to the output
             _current.copyInto(out)
         }
     }
