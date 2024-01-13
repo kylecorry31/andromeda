37959c1cd6d61f503ad59743d2b0d651b3dcbaee
Kyle Corry <kylecorry31@gmail.com>
2024-01-13 07:23:23 -0500

    Add workaround for FGS bug in TileService

--- background/src/main/java/com/kylecorry/andromeda/background/services/AndromedaTileService.kt
+++ background/src/main/java/com/kylecorry/andromeda/background/services/AndromedaTileService.kt
@@ -1,5 +1,6 @@
 package com.kylecorry.andromeda.background.services
 
+import android.app.Dialog
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.Icon
@@ -10,6 +11,12 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.RequiresApi
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.graphics.drawable.toBitmap
+import com.kylecorry.andromeda.core.coroutines.onMain
+import com.kylecorry.andromeda.core.system.CurrentApp
+import com.kylecorry.andromeda.permissions.Permissions
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @RequiresApi(Build.VERSION_CODES.N)
 abstract class AndromedaTileService : TileService() {
@@ -54,5 +61,53 @@ abstract class AndromedaTileService : TileService() {
         val realIcon = Icon.createWithBitmap(bitmap)
         realIcon.setTint(Color.WHITE)
         setIcon(realIcon)
+    }
+
+    private fun AndromedaTileService.isForegroundWorkaroundNeeded(): Boolean {
+        // The bug only happens on Android 14+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            return false
+        }
+
+        if (CurrentApp.isInForeground(includeForegroundServices = true)) {
+            return false
+        }
+
+        return !Permissions.isIgnoringBatteryOptimizations(this)
+    }
+
+    private inline fun AndromedaTileService.startWorkaround(crossinline action: suspend () -> Unit) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            val dialog = Dialog(this)
+            dialog.setOnShowListener {
+                val scope = CoroutineScope(Dispatchers.Default)
+                scope.launch {
+                    try {
+                        action()
+                    } finally {
+                        onMain {
+                            dialog.dismiss()
+                        }
+                    }
+                }
+            }
+            showDialog(dialog)
+        }
+    }
+
+    /**
+     * Allows the starting of a foreground service.
+     * This is a workaround for a bug in Android 14+, and in some cases the notification panel may be dismissed in order to start the service: https://issuetracker.google.com/issues/299506164
+     * @param action the action which will start a foreground service
+     */
+    fun startForegroundService(action: suspend () -> Unit) {
+        if (isForegroundWorkaroundNeeded()) {
+            startWorkaround {
+                action()
+            }
+        } else {
+            val scope = CoroutineScope(Dispatchers.Default)
+            scope.launch { action() }
+        }
     }
 }
