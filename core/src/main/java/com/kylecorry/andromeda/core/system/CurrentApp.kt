8f76b5a733ff2025d18d5a28740123039297ad39
Kyle Corry <kylecorry31@gmail.com>
2024-01-13 07:22:48 -0500

    Add ability to check if app is in the foreground

--- core/src/main/java/com/kylecorry/andromeda/core/system/CurrentApp.kt
+++ core/src/main/java/com/kylecorry/andromeda/core/system/CurrentApp.kt
@@ -1,6 +1,8 @@
 package com.kylecorry.andromeda.core.system
 
+import android.app.ActivityManager
 import android.content.Context
+import android.os.Build
 
 object CurrentApp {
 
@@ -12,6 +14,13 @@ object CurrentApp {
 
     fun kill() {
         Runtime.getRuntime().exit(0)
+    }
+
+    fun isInForeground(includeForegroundServices: Boolean = true): Boolean {
+        val processInfo = ActivityManager.RunningAppProcessInfo()
+        ActivityManager.getMyMemoryState(processInfo)
+        return processInfo.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND ||
+                (includeForegroundServices && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && processInfo.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND_SERVICE)
     }
 
 }
