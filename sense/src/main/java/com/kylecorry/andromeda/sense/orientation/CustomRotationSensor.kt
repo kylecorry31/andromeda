992e1c0cc48fec23393fef7ac2e37e8102b0185d
Kyle Corry <kylecorry31@gmail.com>
2024-03-09 07:56:29 -0500

    Improve custom rotation sensor

--- sense/src/main/java/com/kylecorry/andromeda/sense/orientation/CustomRotationSensor.kt
+++ sense/src/main/java/com/kylecorry/andromeda/sense/orientation/CustomRotationSensor.kt
@@ -1,45 +1,48 @@
 package com.kylecorry.andromeda.sense.orientation
 
-import android.hardware.SensorManager
+import android.util.Log
 import com.kylecorry.andromeda.core.coroutines.onMain
 import com.kylecorry.andromeda.core.sensors.AbstractSensor
 import com.kylecorry.andromeda.sense.accelerometer.IAccelerometer
-import com.kylecorry.andromeda.sense.compass.ICompass
 import com.kylecorry.andromeda.sense.magnetometer.IMagnetometer
 import com.kylecorry.luna.coroutines.CoroutineQueueRunner
 import com.kylecorry.sol.math.Quaternion
 import com.kylecorry.sol.math.QuaternionMath
-import com.kylecorry.sol.units.Bearing
+import com.kylecorry.sol.math.Vector3Utils
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlin.math.sqrt
+import kotlin.math.absoluteValue
 
 class CustomRotationSensor(
     private val magnetometer: IMagnetometer,
     private val accelerometer: IAccelerometer,
     private val gyro: IGyroscope,
-    private val gyroWeight: Float = 0.998f
+    private val gyroWeight: Float = 0.998f,
+    private val outOfSyncDotThreshold: Float = 0.85f,
+    private val outOfSyncMillisThreshold: Long = 1000,
+    private val badMagnetometerMagnitudeThreshold: Float = 65f, // TODO: Estimate this threshold on initialization
+    private val badAccelerometerMagnitudeThreshold: Float = 20f,
+    private val verbose: Boolean = false
 ) : AbstractSensor(), IOrientationSensor {
 
-    private val rotationMatrix = FloatArray(16)
     private val _quaternion = Quaternion.zero.toFloatArray()
-
     private val lock = Object()
 
     private val scope = CoroutineScope(Dispatchers.Default)
     private val runner = CoroutineQueueRunner()
 
+    private val geomagneticOrientationSensor =
+        CustomGeomagneticRotationSensor(magnetometer, accelerometer)
+
     override fun startImpl() {
         isInitialized = false
-        magnetometer.start(this::onSensorUpdate)
-        accelerometer.start(this::onSensorUpdate)
+        geomagneticOrientationSensor.start(this::onSensorUpdate)
         gyro.start(this::onGyroUpdate)
     }
 
     override fun stopImpl() {
-        magnetometer.stop(this::onSensorUpdate)
-        accelerometer.stop(this::onSensorUpdate)
+        geomagneticOrientationSensor.stop(this::onSensorUpdate)
         gyro.stop(this::onGyroUpdate)
         runner.cancel()
     }
@@ -49,64 +52,89 @@ class CustomRotationSensor(
     private var lastGyro = Quaternion.zero.toFloatArray()
 
     private val temp = FloatArray(4)
+    private val magQuaternion = Quaternion.zero.toFloatArray()
+    private val gyroQuaternion = Quaternion.zero.toFloatArray()
+
+    private var outOfSyncTime = 0L
 
     private suspend fun update() {
         synchronized(lock) {
-            if (!accelerometer.hasValidReading || !magnetometer.hasValidReading) {
+            if (!geomagneticOrientationSensor.hasValidReading) {
                 return
             }
-            SensorManager.getRotationMatrix(
-                rotationMatrix,
-                null,
-                accelerometer.rawAcceleration,
-                magnetometer.rawMagneticField
-            )
 
-            val trace = rotationMatrix[0] + rotationMatrix[5] + rotationMatrix[10]
-            val r = sqrt(1 + trace)
-            val s = 1 / (2 * r)
-            var w = r / 2
-            var x = (rotationMatrix[6] - rotationMatrix[9]) * s
-            var y = (rotationMatrix[8] - rotationMatrix[2]) * s
-            var z = (rotationMatrix[1] - rotationMatrix[4]) * s
+            updateMagQuaternion()
+            updateGyroQuaternion()
 
-            // TODO: Instead of inverting, use the correct values from the rotation matrix
-            temp[0] = x
-            temp[1] = y
-            temp[2] = z
-            temp[3] = w
-            QuaternionMath.inverse(temp, temp)
-            x = temp[0]
-            y = temp[1]
-            z = temp[2]
-            w = temp[3]
-
-            // Calculate the change from the gyro
-            QuaternionMath.subtractRotation(gyro.rawOrientation, lastGyro, temp)
-            lastGyro = gyro.rawOrientation.clone()
-            QuaternionMath.multiply(_quaternion, temp, temp)
-            val gx = temp[0]
-            val gy = temp[1]
-            val gz = temp[2]
-            val gw = temp[3]
+            // If there's a really high magnetic field or acceleration, use the gyro only
+            val dot = QuaternionMath.dot(gyroQuaternion, magQuaternion)
 
             // Complementary filter
-            val alpha = if (isInitialized) {
-                gyroWeight
-            } else {
-                isInitialized = true
-                0f
-            }
-            _quaternion[0] = x * (1 - alpha) + gx * alpha
-            _quaternion[1] = y * (1 - alpha) + gy * alpha
-            _quaternion[2] = z * (1 - alpha) + gz * alpha
-            _quaternion[3] = w * (1 - alpha) + gw * alpha
-            QuaternionMath.normalize(_quaternion, _quaternion)
+            val alpha =
+                if (isInitialized && dot.absoluteValue < outOfSyncDotThreshold) {
+                    if (outOfSyncTime == 0L) {
+                        outOfSyncTime = System.currentTimeMillis()
+                    }
+
+                    // If there's a large acceleration or magnetic field, use the gyro only
+                    val magneticMagnitude = Vector3Utils.magnitude(magnetometer.rawMagneticField)
+                    val accelerationMagnitude =
+                        Vector3Utils.magnitude(accelerometer.rawAcceleration)
+
+                    val outOfSyncDuration = System.currentTimeMillis() - outOfSyncTime
+
+                    if (outOfSyncDuration < outOfSyncMillisThreshold) {
+                        // The sensors recently went out of sync, the gyro is likely more accurate
+                        if (verbose) {
+                            Log.d("CustomRotationSensor", "Out of sync - recent")
+                        }
+                        1f
+                    } else if (magneticMagnitude > badMagnetometerMagnitudeThreshold || accelerationMagnitude > badAccelerometerMagnitudeThreshold) {
+                        // The geomagnetic sensor is likely out of sync, use the gyro
+                        if (verbose) {
+                            Log.d(
+                                "CustomRotationSensor",
+                                "Out of sync - magnetic or acceleration out of sync"
+                            )
+                        }
+                        1f
+                    } else {
+                        // The gyro is likely out of sync, use the geomagnetic sensor
+                        if (verbose) {
+                            Log.d("CustomRotationSensor", "Out of sync - gyro out of sync")
+                        }
+                        0f
+                    }
+                } else if (isInitialized) {
+                    outOfSyncTime = 0L
+                    gyroWeight
+                } else {
+                    outOfSyncTime = 0L
+                    isInitialized = true
+                    0f
+                }
+
+            QuaternionMath.slerp(magQuaternion, gyroQuaternion, alpha, _quaternion)
         }
 
         onMain {
             notifyListeners()
         }
+    }
+
+    private fun updateGyroQuaternion() {
+        // Get the change from the gyro
+        QuaternionMath.subtractRotation(gyro.rawOrientation, lastGyro, temp)
+        lastGyro = gyro.rawOrientation.clone()
+
+        // Rotate the current orientation by the change in gyro
+        QuaternionMath.multiply(_quaternion, temp, gyroQuaternion)
+
+        QuaternionMath.normalize(gyroQuaternion, gyroQuaternion)
+    }
+
+    private fun updateMagQuaternion() {
+        geomagneticOrientationSensor.rawOrientation.copyInto(magQuaternion)
     }
 
     private fun onSensorUpdate(): Boolean {
@@ -136,5 +164,5 @@ class CustomRotationSensor(
         get() = null
 
     override val hasValidReading: Boolean
-        get() = magnetometer.hasValidReading && accelerometer.hasValidReading
+        get() = geomagneticOrientationSensor.hasValidReading
 }
