a958bea7916207e9662b1717796aa1564cd6105e
Kyle Corry <kylecorry31@gmail.com>
2024-06-09 14:28:29 -0400

    Update microphone to better support applications

--- sound/src/main/java/com/kylecorry/andromeda/sound/Microphone.kt
+++ sound/src/main/java/com/kylecorry/andromeda/sound/Microphone.kt
@@ -2,99 +2,80 @@ package com.kylecorry.andromeda.sound
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.media.*
+import android.media.AudioFormat
+import android.media.AudioRecord
+import android.media.MediaRecorder
 import android.media.audiofx.AcousticEchoCanceler
 import android.media.audiofx.AutomaticGainControl
 import android.media.audiofx.NoiseSuppressor
-import com.kylecorry.andromeda.core.sensors.AbstractSensor
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.kylecorry.andromeda.permissions.Permissions
-import kotlin.math.abs
 
 
-class Microphone(private val context: Context, private val sampleRate: Int) : AbstractSensor(),
-    IMicrophone {
+class Microphone(
+    private val context: Context,
+    private val sampleRate: Int,
+    private val audioFormat: Int = AudioFormat.ENCODING_PCM_16BIT,
+    private val channelConfig: Int = AudioFormat.CHANNEL_IN_MONO,
+    private val bufferSize: Int = AudioRecord.getMinBufferSize(
+        sampleRate,
+        channelConfig,
+        audioFormat
+    ),
+    private val enableNoiseSuppression: Boolean = true,
+    private val enableEchoCancellation: Boolean = true,
+    private val enableAutomaticGainControl: Boolean = false
+) {
     private var recorder: AudioRecord? = null
-    private var buffer: ShortArray? = null
-    private var _hasReading = false
 
-    private val bufferLock = Object()
+    fun read(byteArray: ByteArray, offset: Int = 0, size: Int = byteArray.size): Int {
+        return recorder?.read(byteArray, offset, size) ?: 0
+    }
 
-    override val audio: ShortArray?
-        get() {
-            val b = synchronized(bufferLock) {
-                buffer?.clone()
-            }
-            return if (b != null) {
-                _hasReading = true
-                recorder!!.read(b, 0, b.size)
-                b
-            } else {
-                null
-            }
-        }
+    fun read(shortArray: ShortArray, offset: Int = 0, size: Int = shortArray.size): Int {
+        return recorder?.read(shortArray, offset, size) ?: 0
+    }
 
-    override val amplitude: Short?
-        get() {
-            return synchronized(bufferLock) {
-                val b = buffer ?: return@synchronized null
-                var max = 0
-                for (s in b) {
-                    if (abs(s.toInt()) > max) {
-                        max = abs(s.toInt())
-                    }
-                }
-                return@synchronized max.toShort()
-            }
-        }
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun read(floatArray: FloatArray, offset: Int = 0, size: Int = floatArray.size): Int {
+        return recorder?.read(floatArray, offset, size, AudioRecord.READ_BLOCKING) ?: 0
+    }
 
     @SuppressLint("MissingPermission")
-    override fun startImpl() {
+    fun start() {
         if (!Permissions.canRecordAudio(context)) {
             return
         }
 
-        val minSize = AudioRecord.getMinBufferSize(
-            sampleRate,
-            AudioFormat.CHANNEL_IN_MONO,
-            AudioFormat.ENCODING_PCM_16BIT
-        )
-        synchronized(bufferLock) {
-            buffer = ShortArray(minSize)
-        }
         recorder = AudioRecord(
             MediaRecorder.AudioSource.MIC,
             sampleRate,
-            AudioFormat.CHANNEL_IN_MONO,
-            AudioFormat.ENCODING_PCM_16BIT,
-            minSize
+            channelConfig,
+            audioFormat,
+            bufferSize
         )
         recorder?.startRecording()
 
         recorder?.audioSessionId?.let {
             if (AcousticEchoCanceler.isAvailable()) {
                 val echo = AcousticEchoCanceler.create(it)
-                echo.enabled = false
+                echo.enabled = enableEchoCancellation
             }
             if (NoiseSuppressor.isAvailable()) {
                 val noise = NoiseSuppressor.create(it)
-                noise.enabled = false
+                noise.enabled = enableNoiseSuppression
             }
             if (AutomaticGainControl.isAvailable()) {
                 val gain = AutomaticGainControl.create(it)
-                gain.enabled = false
+                gain.enabled = enableAutomaticGainControl
             }
         }
 
     }
 
-    override fun stopImpl() {
+    fun stop() {
         recorder?.stop()
         recorder = null
-        synchronized(bufferLock) {
-            buffer = null
-        }
     }
-
-    override val hasValidReading: Boolean
-        get() = _hasReading
 }
