fedbfbe2631152eff7bd3d478d794c9fc9f1fb43
Kyle Corry <kylecorry31@gmail.com>
2024-06-10 18:51:29 -0400

    Add amplitude to dB helper

--- sound/src/main/java/com/kylecorry/andromeda/sound/AudioUtils.kt
+++ sound/src/main/java/com/kylecorry/andromeda/sound/AudioUtils.kt
@@ -1,5 +1,11 @@
 package com.kylecorry.andromeda.sound
 
+import android.util.Range
+import com.kylecorry.sol.math.SolMath
+import org.jetbrains.annotations.ApiStatus.Experimental
+import kotlin.math.log10
+import kotlin.math.pow
+
 object AudioUtils {
 
     /**
@@ -26,6 +32,36 @@ object AudioUtils {
                 (buffer[i * 2].toInt() and 0xFF or (buffer[i * 2 + 1].toInt() shl 8)).toFloat() / Short.MAX_VALUE
         }
         return out
+    }
+
+    /**
+     * Converts an amplitude [-1, 1] as read from the microphone to a dB value
+     * @param amplitude the amplitude
+     * @param decibelRange the range of decibels the microphone can read
+     * @param sensitivity the sensitivity of the microphone
+     * @param referenceSoundPressure the reference sound pressure in Pa (default is 0.00002f or 20 uPa / 0 dB SPL)
+     * @return the dB value
+     */
+    @Experimental
+    fun amplitudeToDb(
+        amplitude: Float,
+        decibelRange: Range<Float>,
+        sensitivity: Float = 0f,
+        referenceSoundPressure: Float = 0.00002f
+    ): Float {
+        // TODO: Add sensitivity before or after the calculation?
+        val maxDb = (decibelRange.upper + sensitivity).coerceAtLeast(0f)
+        val minDb = (decibelRange.lower + sensitivity).coerceAtLeast(0f)
+
+        // Convert dB to sound pressure
+        val maxSoundPressurePa = referenceSoundPressure * 10f.pow(maxDb / 20)
+        val minSoundPressurePa = referenceSoundPressure * 10f.pow(minDb / 20)
+
+        // Interpolate between the min and max sound pressure
+        val readingSoundPressurePa = SolMath.lerp(amplitude, minSoundPressurePa, maxSoundPressurePa)
+
+        // Convert sound pressure to dB
+        return 20 * log10(readingSoundPressurePa / referenceSoundPressure)
     }
 
 }
