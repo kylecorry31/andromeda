006b076077f1626e8c48ccc7f209679a80887024
Kyle Corry <kylecorry31@gmail.com>
2024-10-13 11:29:42 -0400

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 buildscript {
     extra.apply {
         set("groupId", "com.kylecorry.andromeda")
-        set("versionName", "10.3.0")
+        set("versionName", "10.4.0")
         set("targetVersion", 35)
         set("compileVersion", 35)
     }
