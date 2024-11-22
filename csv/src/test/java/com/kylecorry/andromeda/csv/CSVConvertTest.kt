3877a44566b3c0dbdb7257ed1abcdaa96253bfee
Kyle Corry <kylecorry31@gmail.com>
2024-11-22 18:37:48 -0500

    Update csv library
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- csv/src/test/java/com/kylecorry/andromeda/csv/CSVConvertTest.kt
+++ csv/src/test/java/com/kylecorry/andromeda/csv/CSVConvertTest.kt
@@ -11,11 +11,12 @@ internal class CSVConvertTest {
         val data = listOf(
             listOf("Number", "Decimal", "String", "Escape"),
             listOf(1, 3.14, "Test", "Testing, \"123\""),
-            listOf(2, 4.14, "Test1", "Testing1, \"123\"")
+            listOf(2, 4.14, "Test1", "Testing1, \"123\""),
+            listOf(3, 5.14, "Tes\nt2", "Testing2, \"123\"")
         )
 
         val actual = CSVConvert.toCSV(data)
-        val expected = "Number,Decimal,String,Escape\r\n1,3.14,Test,\"Testing, \"\"123\"\"\"\r\n2,4.14,Test1,\"Testing1, \"\"123\"\"\"\r\n"
+        val expected = "Number,Decimal,String,Escape\r\n1,3.14,Test,\"Testing, \"\"123\"\"\"\r\n2,4.14,Test1,\"Testing1, \"\"123\"\"\"\r\n3,5.14,\"Tes\nt2\",\"Testing2, \"\"123\"\"\"\r\n"
 
         assertEquals(expected, actual)
 
