619ce038fc7f525313862f7d8abeab39667af697
Kyle Corry <kylecorry31@gmail.com>
2025-01-31 14:07:16 -0500

    Update version number
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
 import com.vanniktech.maven.publish.SonatypeHost
 
-val versionName = "12.3.0"
+val versionName = "12.4.0"
 val groupId = "com.kylecorry.andromeda"
 
 buildscript {
