00953c0ea317c1bac123fcad99146be15c471eea
Kyle Corry <kylecorry31@gmail.com>
2025-02-03 18:35:28 -0500

    Update version number

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
 import com.vanniktech.maven.publish.SonatypeHost
 
-val versionName = "12.5.0"
+val versionName = "12.6.0"
 val groupId = "com.kylecorry.andromeda"
 
 buildscript {
