7b6b8c595b04c891681b30a401b9e387c3e11ceb
Kyle Corry <kylecorry31@gmail.com>
2025-05-17 06:57:01 -0400

    Add support for dual channel sound
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- sound/src/main/java/com/kylecorry/andromeda/sound/SoundGenerator.kt
+++ sound/src/main/java/com/kylecorry/andromeda/sound/SoundGenerator.kt
@@ -63,4 +63,65 @@ class SoundGenerator {
         track.setLoopPoints(0, size, Int.MAX_VALUE)
         return track
     }
+
+    fun getDualChannelSound(
+        sampleRate: Int = 64000,
+        durationSeconds: Float = 1f,
+        leftSampleGenerator: (i: Int) -> Double,
+        rightSampleGenerator: (i: Int) -> Double
+    ): AudioTrack {
+        // Adapted from https://stackoverflow.com/questions/2413426/playing-an-arbitrary-tone-with-android
+        val size = (durationSeconds * sampleRate).toInt()
+        val sound = ByteArray(4 * size)
+
+        for (i in 0 until size) {
+            val leftSample = leftSampleGenerator(i)
+            val rightSample = rightSampleGenerator(i)
+            val leftPcmSound = (leftSample * 32767).toInt()
+            val rightPcmSound = (rightSample * 32767).toInt()
+            sound[i * 4] = (leftPcmSound and 0x00ff).toByte()
+            sound[i * 4 + 1] = ((leftPcmSound and 0xff00) shr 8).toByte()
+            sound[i * 4 + 2] = (rightPcmSound and 0x00ff).toByte()
+            sound[i * 4 + 3] = ((rightPcmSound and 0xff00) shr 8).toByte()
+        }
+
+        val track = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            AudioTrack.Builder()
+                .setAudioAttributes(
+                    AudioAttributes.Builder()
+                        .setUsage(AudioAttributes.USAGE_MEDIA)
+                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                        .build()
+                )
+                .setAudioFormat(
+                    AudioFormat.Builder()
+                        .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                        .setSampleRate(sampleRate)
+                        .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO)
+                        .build()
+                )
+                .setBufferSizeInBytes(sound.size)
+                .setTransferMode(AudioTrack.MODE_STATIC)
+                .build()
+        } else {
+            AudioTrack(
+                AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .build(),
+                AudioFormat.Builder()
+                    .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                    .setSampleRate(sampleRate)
+                    .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO)
+                    .build(),
+                sound.size,
+                AudioTrack.MODE_STATIC,
+                AudioManager.AUDIO_SESSION_ID_GENERATE
+            )
+        }
+
+        track.write(sound, 0, sound.size)
+        track.setLoopPoints(0, size, Int.MAX_VALUE)
+        return track
+    }
 }
