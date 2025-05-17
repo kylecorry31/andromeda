43f7ba70d9a7da180be9f48b292f674078b2dd57
Kyle Corry <kylecorry31@gmail.com>
2025-05-17 07:43:39 -0400

    Update dependencies and enable 16KB support
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
 import com.vanniktech.maven.publish.SonatypeHost
 
-val versionName = "14.1.0"
+val versionName = "14.2.0"
 val groupId = "com.kylecorry.andromeda"
 
 buildscript {
