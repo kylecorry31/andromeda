d920ae903b91add6f1a29527dde2f966e3cb2297
Kyle Corry <kylecorry31@gmail.com>
2025-09-09 18:09:10 -0400

    Improve 5G detection
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- signal/src/main/java/com/kylecorry/andromeda/signal/CellSignalSensor.kt
+++ signal/src/main/java/com/kylecorry/andromeda/signal/CellSignalSensor.kt
@@ -12,7 +12,12 @@ import android.telephony.CellInfoLte
 import android.telephony.CellInfoNr
 import android.telephony.CellInfoTdscdma
 import android.telephony.CellInfoWcdma
+import android.telephony.CellSignalStrengthNr
+import android.telephony.SignalStrength
+import android.telephony.TelephonyCallback
+import android.telephony.TelephonyDisplayInfo
 import android.telephony.TelephonyManager
+import androidx.core.content.ContextCompat
 import androidx.core.content.getSystemService
 import androidx.core.math.MathUtils
 import com.kylecorry.andromeda.core.coroutines.SafeExecutor
@@ -33,6 +38,37 @@ class CellSignalSensor(
     AbstractSensor(), ICellSignalSensor {
 
     private val telephony by lazy { context.getSystemService<TelephonyManager>() }
+    private val executor by lazy { ContextCompat.getMainExecutor(context) }
+    private var isAlsoConnectedTo5g = false
+    private var dbmSecondary5g: Int? = null
+    private var levelSecondary5g: Int? = null
+    private var timeSecondary5g: Instant? = null
+    private val telephonyCallback = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        object : TelephonyCallback(),
+            TelephonyCallback.DisplayInfoListener,
+            TelephonyCallback.SignalStrengthsListener {
+            override fun onDisplayInfoChanged(telephonyDisplayInfo: TelephonyDisplayInfo) {
+                isAlsoConnectedTo5g = telephonyDisplayInfo.overrideNetworkType in listOf(
+                    TelephonyDisplayInfo.OVERRIDE_NETWORK_TYPE_NR_NSA,
+                    TelephonyDisplayInfo.OVERRIDE_NETWORK_TYPE_NR_ADVANCED
+                )
+                updateSignals(notify = true)
+            }
+
+            override fun onSignalStrengthsChanged(signalStrength: SignalStrength) {
+                val nrSignal = signalStrength.cellSignalStrengths.firstOrNull {
+                    it is CellSignalStrengthNr
+                }
+
+                dbmSecondary5g = nrSignal?.dbm
+                levelSecondary5g = nrSignal?.level
+                timeSecondary5g = if (nrSignal != null) Instant.now() else null
+                updateSignals(notify = true)
+            }
+        }
+    } else {
+        null
+    }
 
     override val hasValidReading: Boolean
         get() = hasReading
@@ -92,82 +128,93 @@ class CellSignalSensor(
 
     @Suppress("DEPRECATION")
     private fun updateCellInfo(cells: List<CellInfo>, notify: Boolean = true) {
+        val newSignals =
+            cells.filter { !removeUnregisteredSignals || it.isRegistered }.mapNotNull {
+                when {
+                    it is CellInfoWcdma -> {
+                        RawCellSignal(
+                            it.cellIdentity.cid.toString(),
+                            getTimestamp(it),
+                            it.cellSignalStrength.dbm,
+                            it.cellSignalStrength.level,
+                            CellNetwork.Wcdma,
+                            it.isRegistered,
+                            it.isConnected()
+                        )
+                    }
+
+                    it is CellInfoGsm -> {
+                        RawCellSignal(
+                            it.cellIdentity.cid.toString(),
+                            getTimestamp(it),
+                            it.cellSignalStrength.dbm,
+                            it.cellSignalStrength.level,
+                            CellNetwork.Gsm,
+                            it.isRegistered,
+                            it.isConnected()
+                        )
+                    }
+
+                    it is CellInfoLte -> {
+                        RawCellSignal(
+                            it.cellIdentity.ci.toString(),
+                            getTimestamp(it),
+                            it.cellSignalStrength.dbm,
+                            it.cellSignalStrength.level,
+                            CellNetwork.Lte,
+                            it.isRegistered,
+                            it.isConnected()
+                        )
+                    }
+
+                    it is CellInfoCdma -> {
+                        RawCellSignal(
+                            it.cellIdentity.basestationId.toString(),
+                            getTimestamp(it),
+                            it.cellSignalStrength.dbm,
+                            it.cellSignalStrength.level,
+                            CellNetwork.Cdma,
+                            it.isRegistered,
+                            it.isConnected()
+                        )
+                    }
+
+                    Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && it is CellInfoTdscdma -> {
+                        RawCellSignal(
+                            it.cellIdentity.cid.toString(),
+                            getTimestamp(it),
+                            it.cellSignalStrength.dbm,
+                            it.cellSignalStrength.level,
+                            CellNetwork.Tdscdma,
+                            it.isRegistered,
+                            it.isConnected()
+                        )
+                    }
+
+                    Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && it is CellInfoNr -> {
+                        RawCellSignal(
+                            it.cellIdentity.operatorAlphaLong.toString(),
+                            getTimestamp(it),
+                            it.cellSignalStrength.dbm,
+                            it.cellSignalStrength.level,
+                            CellNetwork.Nr,
+                            it.isRegistered,
+                            it.isConnected()
+                        )
+                    }
+
+                    else -> null
+                }
+            }
+
+        updateSignals(newSignals, notify)
+    }
+
+    private fun updateSignals(raw: List<RawCellSignal>? = null, notify: Boolean = true) {
         synchronized(this) {
             hasReading = true
-            val newSignals =
-                cells.filter { !removeUnregisteredSignals || it.isRegistered }.mapNotNull {
-                    when {
-                        it is CellInfoWcdma -> {
-                            RawCellSignal(
-                                it.cellIdentity.cid.toString(),
-                                getTimestamp(it),
-                                it.cellSignalStrength.dbm,
-                                it.cellSignalStrength.level,
-                                CellNetwork.Wcdma,
-                                it.isRegistered
-                            )
-                        }
 
-                        it is CellInfoGsm -> {
-                            RawCellSignal(
-                                it.cellIdentity.cid.toString(),
-                                getTimestamp(it),
-                                it.cellSignalStrength.dbm,
-                                it.cellSignalStrength.level,
-                                CellNetwork.Gsm,
-                                it.isRegistered
-                            )
-                        }
-
-                        it is CellInfoLte -> {
-                            RawCellSignal(
-                                it.cellIdentity.ci.toString(),
-                                getTimestamp(it),
-                                it.cellSignalStrength.dbm,
-                                it.cellSignalStrength.level,
-                                CellNetwork.Lte,
-                                it.isRegistered
-                            )
-                        }
-
-                        it is CellInfoCdma -> {
-                            RawCellSignal(
-                                it.cellIdentity.basestationId.toString(),
-                                getTimestamp(it),
-                                it.cellSignalStrength.dbm,
-                                it.cellSignalStrength.level,
-                                CellNetwork.Cdma,
-                                it.isRegistered
-                            )
-                        }
-
-                        Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && it is CellInfoTdscdma -> {
-                            RawCellSignal(
-                                it.cellIdentity.cid.toString(),
-                                getTimestamp(it),
-                                it.cellSignalStrength.dbm,
-                                it.cellSignalStrength.level,
-                                CellNetwork.Tdscdma,
-                                it.isRegistered
-                            )
-                        }
-
-                        Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && it is CellInfoNr -> {
-                            RawCellSignal(
-                                it.cellIdentity.operatorAlphaLong.toString(),
-                                getTimestamp(it),
-                                it.cellSignalStrength.dbm,
-                                it.cellSignalStrength.level,
-                                CellNetwork.Nr,
-                                it.isRegistered
-                            )
-                        }
-
-                        else -> null
-                    }
-                }
-
-            val latestSignals = newSignals.map {
+            var latestSignals = (raw ?: oldSignals).map {
                 val old = oldSignals.find { signal -> it.id == signal.id }
                 if (old == null) {
                     it
@@ -175,6 +222,8 @@ class CellSignalSensor(
                     if (old.time > it.time) old else it
                 }
             }
+
+            latestSignals = applyOverrides(latestSignals)
 
             oldSignals = latestSignals
 
@@ -196,6 +245,27 @@ class CellSignalSensor(
         }
     }
 
+    private fun applyOverrides(signals: List<RawCellSignal>): List<RawCellSignal> {
+        if (!isAlsoConnectedTo5g) {
+            return signals
+        }
+        val connected5g = signals.firstOrNull { it.network == CellNetwork.Nr && it.isConnected }
+        val connected = signals.firstOrNull { it.isConnected }
+
+        // Add the 5G override
+        return signals.filter { it.network != CellNetwork.Nr || !it.isConnected } + listOf(
+            RawCellSignal(
+                connected5g?.id ?: "${connected?.id ?: "unknown"}-5g",
+                timeSecondary5g ?: connected5g?.time ?: connected?.time ?: Instant.now(),
+                dbmSecondary5g ?: connected5g?.dbm ?: connected?.dbm ?: 0,
+                levelSecondary5g ?: connected5g?.level ?: connected?.level ?: 0,
+                CellNetwork.Nr,
+                isRegistered = true,
+                isConnected = true
+            )
+        )
+    }
+
     @SuppressLint("MissingPermission")
     override fun startImpl() {
         if (!Permissions.canGetFineLocation(context)) {
@@ -203,7 +273,11 @@ class CellSignalSensor(
             notifyListeners()
             return
         }
+
         loadFromCache(false)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && telephonyCallback != null) {
+            telephony?.registerTelephonyCallback(executor, telephonyCallback)
+        }
         intervalometer.interval(Duration.ofSeconds(5))
     }
 
@@ -226,6 +300,17 @@ class CellSignalSensor(
 
     override fun stopImpl() {
         intervalometer.stop()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && telephonyCallback != null) {
+            telephony?.unregisterTelephonyCallback(telephonyCallback)
+        }
+    }
+
+    private fun CellInfo.isConnected(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            this.cellConnectionStatus == CellInfo.CONNECTION_PRIMARY_SERVING || this.cellConnectionStatus == CellInfo.CONNECTION_SECONDARY_SERVING
+        } else {
+            false
+        }
     }
 
     data class RawCellSignal(
@@ -234,7 +319,8 @@ class CellSignalSensor(
         val dbm: Int,
         val level: Int,
         val network: CellNetwork,
-        val isRegistered: Boolean
+        val isRegistered: Boolean,
+        val isConnected: Boolean
     ) {
         val percent: Float
             get() {
