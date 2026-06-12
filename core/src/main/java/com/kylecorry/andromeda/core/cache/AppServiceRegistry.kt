b120b86c6040cf33d76f6e662876bd9d743f175a
Kyle Corry <kylecorry31@gmail.com>
2026-06-12 13:47:14 -0400

    Add lazy registration to AppServiceRegistry
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- core/src/main/java/com/kylecorry/andromeda/core/cache/AppServiceRegistry.kt
+++ core/src/main/java/com/kylecorry/andromeda/core/cache/AppServiceRegistry.kt
@@ -2,15 +2,36 @@ package com.kylecorry.andromeda.core.cache
 
 object AppServiceRegistry {
 
-    val services = mutableMapOf<String, Any>()
+    private val services = mutableMapOf<Class<*>, Lazy<Any>>()
 
     inline fun <reified T : Any> register(service: T) {
-        services[T::class.java.name] = service
+        register(T::class.java, service)
+    }
+
+    fun <T : Any> register(`class`: Class<T>, service: T) {
+        services[`class`] = lazyOf(service)
+    }
+
+    fun <T : Any> register(`class`: Class<T>, service: () -> T) {
+        services[`class`] = lazy { service() }
+    }
+
+    inline fun <reified T : Any> unregister() {
+        unregister(T::class.java)
+    }
+
+    fun <T : Any> unregister(`class`: Class<T>) {
+        services.remove(`class`)
     }
 
     inline fun <reified T : Any> get(): T {
-        return services[T::class.java.name] as? T
-            ?: throw Exception("Service is not of the correct type")
+        return get(T::class.java)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T : Any> get(`class`: Class<T>): T {
+        return services[`class`]?.value as? T
+            ?: throw IllegalStateException("No service registered for ${`class`.name}")
     }
 
 }
