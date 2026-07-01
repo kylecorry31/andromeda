8c37c521266171510ee175ddb37f9b81fff52814
Kyle Corry <kylecorry31@gmail.com>
2026-07-01 10:38:19 -0400

    Interpolate alpha in Colors.interpolate
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- core/src/main/java/com/kylecorry/andromeda/core/ui/Colors.kt
+++ core/src/main/java/com/kylecorry/andromeda/core/ui/Colors.kt
@@ -76,16 +76,19 @@ object Colors {
 
     @ColorInt
     fun interpolate(@ColorInt color1: Int, @ColorInt color2: Int, factor: Float): Int {
+        val a1 = Color.alpha(color1)
         val r1 = Color.red(color1)
         val g1 = Color.green(color1)
         val b1 = Color.blue(color1)
+        val a2 = Color.alpha(color2)
         val r2 = Color.red(color2)
         val g2 = Color.green(color2)
         val b2 = Color.blue(color2)
+        val a = (a1 + ((a2 - a1) * factor)).toInt()
         val r = (r1 + ((r2 - r1) * factor)).toInt()
         val g = (g1 + ((g2 - g1) * factor)).toInt()
         val b = (b1 + ((b2 - b1) * factor)).toInt()
-        return Color.rgb(r, g, b)
+        return Color.argb(a, r, g, b)
     }
 
     fun fromColorTemperature(temp: Float): Int {
